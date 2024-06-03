c870b76398b9957c9835a25f9695d446bf6c5aa1
Vladimir <121644805+polyarn1y@users.noreply.github.com>
2024-06-03 11:12:22 +0300

    fix

--- javascript/lab1/script3.js
+++ javascript/lab1/script3.js
@@ -2,10 +2,13 @@
 
 let num;
 
-do {
+while (true) {
     num = prompt("Введите число больше 100:");
-} while (isNaN(num) || num <= 100);
-
-if (num == null) { 
-    alert("Вы отменили выполнение скрипта");
+    if (num === null) {
+        alert("Вы отменили выполнение скрипта");
+        break;
+    } else if (!isNaN(num) && num > 100) {
+        alert(`Вы ввели число: ${num}`);
+        break;
+    }
 }
